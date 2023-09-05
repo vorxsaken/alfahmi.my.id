756ecbcdb9c0d98fdca343a8bf96df83db86c305
vorxsaken <jhonyevans6326@gmail.com>
2023-09-05 23:58:51 +0700

    chore: change transition out variable

--- animations/Transition.tsx
+++ animations/Transition.tsx
@@ -7,55 +7,55 @@ export default function Transition({skipOutro}: {skipOutro?: boolean}) {
             from={{ yPercent: 0 }}
             to={{ yPercent: -100 }}
             delayIn={0.5}
-            delayOut={0.5}
+            delayOut={0}
             durationIn={0.5}
             skipOutro={skipOutro || false}
             ease={'power4.inOut'}
-            durationOut={0.5}
+            durationOut={0.1}
             className="w-screen h-screen flex flex-row fixed top-0 z-50"
         >
             <AnimateInOut
                 from={{ yPercent: 0 }}
                 to={{ yPercent: -100 }}
                 delayIn={0}
-                delayOut={0.6}
+                delayOut={0.15}
                 durationIn={1}
                 skipOutro={skipOutro || false}
                 ease={'power2.inOut'}
-                durationOut={1}
+                durationOut={0.5}
                 className="w-[25%] h-screen z-50 bg-[#BCE6EB]">
             </AnimateInOut>
             <AnimateInOut
                 from={{ yPercent: 0 }}
                 to={{ yPercent: -100 }}
                 delayIn={0.1}
-                delayOut={0.7}
+                delayOut={0.2}
                 durationIn={1}
                 skipOutro={skipOutro || false}
                 ease={'power2.inOut'}
-                durationOut={1}
+                durationOut={0.5}
                 className="w-[25%] h-screen z-50 bg-[#FDCFDF]">
             </AnimateInOut>
             <AnimateInOut
                 from={{ yPercent: 0 }}
                 to={{ yPercent: -100 }}
                 delayIn={0.2}
-                delayOut={0.8}
+                delayOut={0.25}
                 durationIn={1}
                 skipOutro={skipOutro || false}
                 ease={'power2.inOut'}
-                durationOut={1}
+                durationOut={0.5}
                 className="w-[25%] h-screen z-50 bg-[#FBBEDF]">
             </AnimateInOut>
             <AnimateInOut
                 from={{ yPercent: 0 }}
                 to={{ yPercent: -100 }}
                 delayIn={0.3}
-                delayOut={0.9}
+                delayOut={0.3}
                 durationIn={1}
                 skipOutro={skipOutro || false}
                 ease={'power2.inOut'}
-                durationOut={1}
+                durationOut={0.5}
                 className="w-[25%] h-screen z-50 bg-[#FCA3CC]">
             </AnimateInOut>
         </AnimateInOut>
